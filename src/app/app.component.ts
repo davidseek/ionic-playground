9e93d1a23584632af3b3d4571ca7e9c54b07636e
David Seek <davidseek1986@gmail.com>
2019-09-19 10:49:52 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {
+  Platform
+} from '@ionic/angular';
+import {
+  SplashScreen
+} from '@ionic-native/splash-screen/ngx';
+import {
+  StatusBar
+} from '@ionic-native/status-bar/ngx';
+import {
+  HttpClient,
+  HttpHeaders
+} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +25,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private httpClient: HttpClient
   ) {
     this.initializeApp();
   }
@@ -22,6 +35,15 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.httpClient.get('https://httpbin.org/image', {
+        headers: new HttpHeaders({
+          accept: 'image/*',
+        }),
+        responseType: 'blob'
+      }).subscribe((data) => {
+        console.log('Data', data);
+      });
     });
   }
 }
